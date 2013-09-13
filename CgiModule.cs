fa38e99d209c282e53def42fa14cbb12322ddc03
Yaroslav Sivakov <yar3333@gmail.com>
2013-09-13 10:25:10 +0400

    + init

---
+++ CgiModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebDesk
+{
+    class CgiModule : HttpServer.Modules.IModule
+    {
+        public HttpServer.ProcessingResult Process(HttpServer.RequestContext context)
+        {
+            //context.Response.Body.Write();
+            return HttpServer.ProcessingResult.SendResponse;
+        }
+    }
+}
